202915ee7e90273665d7a9426f6a749f8c893c7c
Yulia Osadchuk <yulia.osadchuk@gmail.com>
2020-10-28 20:38:25 +0200

    add Stage 3/5: What's up on the field?

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,6 +1,5 @@
 package tictactoe;
 
-import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Main {
@@ -10,27 +9,94 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter cells: ");
         String s = scanner.next();
-        char[] array = s.toCharArray();
-        //printArray(array);
-        checkEmptyCells(array);
+        char[][] array = new char[3][3];
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = s.charAt(index);
+                index++;
+            }
+        }
+        printArray(array);
+        checkWinXOrO(array);
         System.out.println(status);
     }
 
-    public static void printArray(char[] array) {
+    public static void printArray(char[][] array) {
         String start = "---------";
         System.out.println(start);
-        System.out.println("| " + array[0] + " " + array[1] + " " + array[2] + " |");
-        System.out.println("| " + array[3] + " " + array[4] + " " + array[5] + " |");
-        System.out.println("| " + array[6] + " " + array[7] + " " + array[8] + " |");
+        for (char[] c : array) {
+            System.out.print("| ");
+            for (char ch : c) {
+                System.out.print(ch + " ");
+            }
+            System.out.println("|");
+        }
         System.out.println(start);
     }
 
-    public static void checkEmptyCells(char[] array) {
-        for (char i : array) {
-            if (i == 95) {
-                status = "Game not finished";
-                return;
+    public static void checkWinXOrO(char[][] array) {
+        boolean xWin = false;
+        boolean oWin = false;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i][0] == 88 && array[i][0] == array[i][1] && array[i][0] == array[i][2] ||
+                    array[0][i] == 88 && array[0][i] == array[1][i] && array[0][i] == array[2][i]) {
+                xWin = true;
             }
         }
+        if (array[0][0] == 88 && array[0][0] == array[1][1] && array[0][0] == array[2][2] ||
+                array[0][2] == 88 && array[0][2] == array[1][1] && array[0][2] == array[2][0]) {
+            xWin = true;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i][0] == 79 && array[i][0] == array[i][1] && array[i][0] == array[i][2] ||
+                    array[0][i] == 79 && array[0][i] == array[1][i] && array[0][i] == array[2][i]) {
+                oWin = true;
+            }
+        }
+        if (array[0][0] == 79 && array[0][0] == array[1][1] && array[0][0] == array[2][2] ||
+                array[0][2] == 79 && array[0][2] == array[1][1] && array[0][2] == array[2][0]) {
+            oWin = true;
+        }
+
+        if (xWin && oWin) {
+            status = "Impossible";
+        } else if (xWin) {
+            status = "X wins";
+        } else if (oWin) {
+            status = "O wins";
+        } else {
+            checkEmptyCells(array);
+        }
+    }
+
+
+    public static void checkEmptyCells(char[][] array) {
+        int x = 0;
+        int o = 0;
+        for (char[] i : array) {
+            for (char j : i) {
+                if (j == 88) {
+                    x++;
+                } else if (j == 79) {
+                    o++;
+                }
+            }
+        }
+        if (x - o >= 2 || o - x >= 2) {
+            status = "Impossible";
+            return;
+        }
+
+        for (char[] i : array) {
+            for (char j : i) {
+                if (j == 95) {
+                    status = "Game not finished";
+                    return;
+                }
+            }
+        }
+        status = "Draw";
     }
 }
